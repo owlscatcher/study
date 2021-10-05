c19e31e8a1c1f035a77a3eb8dfa188fa56f7ec54
Alexandr Veselov <collabsis.g@gmail.com>
2021-10-05 20:28:11 +0400

    Completed exercise: Fp2 (my_each, my_map, my_compact, my_reduce)

--- test/exercise/fp2/solution.rb
+++ test/exercise/fp2/solution.rb
@@ -5,16 +5,29 @@ module Exercise
       # Использовать свои написанные функции для реализации следующих - можно.
 
       # Написать свою функцию my_each
-      def my_each; end
+      def my_each
+        for element in self
+          yield element
+        end
+      end
 
       # Написать свою функцию my_map
-      def my_map; end
+      def my_map
+        my_reduce(MyArray.new) { |acc, element| acc << yield(element) }
+      end
 
       # Написать свою функцию my_compact
-      def my_compact; end
+      def my_compact
+        my_reduce(MyArray.new) { |acc, element| element.nil? ? acc : acc << element }
+      end
 
       # Написать свою функцию my_reduce
-      def my_reduce; end
+      def my_reduce(acc = nil)
+        my_each do |element|
+          acc = acc.nil? ? element : yield(acc, element)
+        end
+        acc
+      end
     end
   end
 end
